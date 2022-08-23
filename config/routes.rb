201a0243040b82e7c2f80a9dff1644a38254b9f9
Jean Paul Fortuno <jp.fortuno@nullpod.com>
2022-08-23 10:15:21 +0400

    Added CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+
+  # get "restaurants", to: "restaurants#index", as: :restaurants
+  # post "restaurants", to: "restaurants#create"
+
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
+  # get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  # patch "restaurants/:id", to: "restaurants#update"
+
+  # delete "restaurants/:id", to: "restaurants#destroy"
+
 end
