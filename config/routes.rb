40ac6699394af91db76039d43a354d5e3a26737c
Jean Paul Fortuno <jp.fortuno@nullpod.com>
2022-08-23 10:28:21 +0400

    Refatoring code

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,3 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # get "restaurants/new", to: "restaurants#new", as: :new_restaurant
-
-  # get "restaurants", to: "restaurants#index", as: :restaurants
-  # post "restaurants", to: "restaurants#create"
-
-  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
-
-  # get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
-  # patch "restaurants/:id", to: "restaurants#update"
-
-  # delete "restaurants/:id", to: "restaurants#destroy"
-
+  resources :restaurants
 end
